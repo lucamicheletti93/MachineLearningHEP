f336a82a30e4c8f83110e363e287e18fe3d00ce9
Luca Micheletti <lmichele@aliceml.cern.ch>
2021-10-20 13:36:23 +0200

    Small changes for the X optimization

--- machine_learning_hep/run5.sh
+++ machine_learning_hep/run5.sh
@@ -8,6 +8,8 @@ rm -rf  /data/X3872ALICE3/newvariables/mc/pklskmldata_scenario3
 rm -rf  /data/X3872ALICE3/newvariables/data/pklskmldata_scenario3
 rm -rf  /data/X3872ALICE3/newvariables/mc/mltotdata_scenario3
 rm -rf  /data/X3872ALICE3/newvariables/data/mltotdata_scenario3
+rm -rf  mlout/ 
+rm -rf  mlplot/
 
 DISPLAY="" python do_entire_analysis.py -r default_complete.yml -d data/data_run5/database_ml_parameters_Xanalysis_scenario3.yml -a scenario3
 
